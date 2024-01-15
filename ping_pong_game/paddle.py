b9a025a723c60e4e8b4ed37e15678b82b9839567
unknown <denisedubois93@gmail.com>
2024-01-15 20:02:41 -0300

    player gets a point when the other paddle misses

--- ping_pong_game/paddle.py
+++ ping_pong_game/paddle.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+# La clase Paddle crea un objeto tortuga de forma cuadrada con un color blanco y establece su tamaño,
+# posición y estado de penup.
 class Paddle(Turtle):
     def __init__(self, position) -> None:
         super().__init__()
@@ -10,9 +12,15 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
+        """
+        La función "go_up" mueve un objeto hacia arriba 28 unidades.
+        """
         new_y = self.ycor() + 28
         self.goto(self.xcor(), new_y)
 
     def go_down(self):
+        """
+        La función mueve un objeto hacia abajo 28 unidades.
+        """
         new_y = self.ycor() - 28
         self.goto(self.xcor(), new_y)
